f3d777b16dd8575cd1e23b663e4d70e3cd4f33d6
marcog83 <marcogobbi0@gmail.com>
2016-11-13 19:01:17 +0100

    search-toolbar

--- static-web/js/definitions.js
+++ static-web/js/definitions.js
@@ -1,7 +1,7 @@
 define(function (require, exports, module) {
     'use strict';
     module.exports = {
-        "perfect-scrollbar": "modules/modules/perfect-scrollbar/perfect-scrollbar"
+        "perfect-scrollbar": "modules/perfect-scrollbar/perfect-scrollbar"
         , "search-toolbar": "modules/search/search-toolbar"
         , "map": "modules/map/map"
     };
