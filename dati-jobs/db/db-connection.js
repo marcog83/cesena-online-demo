cbdb3edb8cd47eda17c1bae6da118435b3b9ae63
marcog83 <marcogobbi0@gmail.com>
2016-11-21 20:16:35 +0100

    config

--- dati-jobs/db/db-connection.js
+++ dati-jobs/db/db-connection.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/cesena-sociale';
+// var url = 'mongodb://localhost:27017/cesena-sociale';
+var url = 'mongodb://marcogobbi:m4rc0g0bb1@ds159527.mlab.com:59527/cesena-sociale';
 
 
 var Deferred = function () {
