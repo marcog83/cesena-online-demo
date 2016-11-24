6d4fa69f054fec4df47bddaee5d1a5de164599ea
marcog83 <marcogobbi0@gmail.com>
2016-11-24 20:04:21 +0100

    sitemap.xml

--- fe-web/server.js
+++ fe-web/server.js
@@ -67,6 +67,7 @@ app.use(bodyParser.urlencoded({extended: true, keepExtensions: true})); // for p
 
 handlebars(app);
 app.use("/google2b3a4456558dadd3.html", express.static(__dirname + '/google2b3a4456558dadd3.html'));
+app.use("/sitemap.xml", express.static(__dirname + '/sitemap.xml'));
 app.use('/wireframes', express.static(__dirname + '/../wireframes',{
     maxAge: '5d'
     ,etag:"strong"
