aca9926c5c55330948b59712e9fbd32745682531
marcog83 <marcogobbi0@gmail.com>
2016-11-25 11:45:36 +0100

    service worker regexp

--- static-web/js/modules/service-worker/service-worker-browser.js
+++ static-web/js/modules/service-worker/service-worker-browser.js
@@ -7,10 +7,14 @@ define(function (require) {
     return function(node,dispatcher){
         if("serviceWorker" in navigator){
             var url_sw="/static/js/modules/service-worker/service-worker.js";//;cf.provide(cf.Type.CONFIGURATION).SERVICE_WORKER_URL;
-            navigator.serviceWorker.register(url_sw,{ scope: '/' }).catch(function(a) {console.log(a)});
+            navigator.serviceWorker.register(url_sw,{ scope: '/' })
+                .then(function(registration){
+                    registration.update();
+                })
+                .catch(function(a) {console.log(a)});
             navigator.serviceWorker.ready
-                .then(function(a) {console.log(a)})
-                .catch(function(a) {console.log(a)})
+                .then(function(a) {console.log("ready",a)})
+                .catch(function(a) {console.log("ready catch",a)})
         }
     };
 });
